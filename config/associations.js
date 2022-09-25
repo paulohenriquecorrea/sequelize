7a6bc26572e1cc2e4b70ae929968718976299755
Paulo Henrique Corrêa Lemos <=>
2022-09-25 14:33:39 -0400

    Adicionado relacionamento hasMany (arquivo /config/associations.js)

--- config/associations.js
+++ config/associations.js
@@ -1,8 +1,13 @@
 const Planet = require("../models/Planet");
 const Satelite = require("../models/Satelite");
 
+/*
 Planet.hasOne(Satelite, {onDelete: "CASCADE", onUpdate: "CASCADE"});
 Satelite.belongsTo(Planet, {foreingKey: "planetId", as: "planet"}); 
 // o 'as: "planet"' cria a chave estrangeira planetId
+*/
 
+
+Planet.hasMany(Satelite, {onDelete: "CASCADE", onUpdate: "CASCADE"});
+Satelite.belongsTo(Planet, {foreingKey: "planetId", as: "planet"}); 
 module.exports = {Planet, Satelite};
